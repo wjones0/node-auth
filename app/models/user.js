1553f2b8b4352b9fd5383edfee31abf3c6c8c85b
William Jones <wjones0@users.noreply.github.com>
2015-09-20 17:11:49 -0400

    create the profile view

--- app/models/user.js
+++ app/models/user.js
@@ -40,7 +40,7 @@ userSchema.methods.generateHash = function(password) {
 	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);	
 };
 
-// check if pw is valie
+// check if pw is valid
 userSchema.methods.validPassword = function(password) {
 	return bcrypt.compareSync(password, this.local.password);	
 };
